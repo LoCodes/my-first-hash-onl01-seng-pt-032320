d99656c039110b9b0b9bba86d724ea134024ed25
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-10 04:10:32 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,15 +1,16 @@
+
+# use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 def my_hash
   my_hash = {"name" => "Loren"}
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-
 end
 
 
+# set a variable called `the_manifest`, equal to a hash
+# fill that hash with key/value pairs that describe the following information:
+# We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
+
+
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information:
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
 
 
 end
