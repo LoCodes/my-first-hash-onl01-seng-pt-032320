81ce7d064939b61847cf282a163b89f4a88acccc
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-10 04:10:59 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 
 # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 def my_hash
-  my_hash = {"name" => "Loren"}
+  my_name = {"name" => "Loren"}
 end
 
 
