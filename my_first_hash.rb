50d5cdd1e52230965f616d4105322a7b282990c4
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-10 04:26:55 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,7 +17,7 @@ end
 
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  puts shipping_manifest["oil paintings"]
+  shipping_manifest["oil paintings"]
 end
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
